c1757597c5c1361a570f6659bd004b85a0a7214b
ninamas <151721366+ninamas@users.noreply.github.com>
2023-12-13 11:12:35 +0100

    Update circuit.py

--- circuit.py
+++ circuit.py
@@ -1,80 +1,44 @@
-import matplotlib.pyplot as plt
 import numpy as np
-import scipy.integrate as integrate
-from scipy.optimize import fsolve
+import matplotlib.pyplot as plt
 
-width= 0.5
-eps= 0.00001
+rayon_circuit = 0.5
+eps = 0.01
 
+
+#Courbe paramétrique centrale
 def f(t):
-    y=t**3-t
-    return np.asarray([t, y])
+    return np.asarray([t, t**3-t])
 
 def f_prime(t):
     return np.asarray([np.ones(len(t)), 3*t**2-1])
 
-params=np.linspace(-1,1,1000)
-pos=f(params)
-speed=f_prime(params)
-
-orth_x=speed[1]
-orth_y=-speed[0]
-orth=np.stack([orth_x,orth_y])
-
-#normalisation vecteurs
-norme=np.sqrt(orth_x**2 + orth_y**2)
-orth_normalise = orth / norme
-
-#affichage vecteurs vitesses
-#plt.quiver(pos[0],pos[1], speed[0], speed[1], angles='xy', color= 'r')
-
-#affichage vecteurs orthogonaux à droite
-#plt.quiver(pos[0],pos[1], orth_normalise[0], orth_normalise[1], angles='xy', color='y')
-
-#à gauche
-#plt.quiver(pos[0],pos[1], -orth_normalise[0], -orth_normalise[1], angles='xy', color='y')
+params = np.linspace(-1.5,1.5, 1000)
+pos = f(params)
+speed = f_prime(params)
+orth_x = speed[1]
+orth_y = -speed[0]
+orth = np.stack([orth_x, orth_y])
 
 
+#Normaliser les vecteurs
+orth_normalise = orth / np.sqrt(orth_x**2 + orth_y**2)
 
-plt.plot(pos[0],pos[1],'b--')
-#bordure droite
-plt.plot(pos[0]+orth_normalise[0]*width , pos[1]+orth_normalise[1]*width, color='k' )
-#bordure gauche
-plt.plot(pos[0]-orth_normalise[0]*width , pos[1]-orth_normalise[1]*width, color='k' )
 
-#gca=get current acces
-plt.gca().set_aspect('equal')
-plt.show()
-
-#detection des points trop proches du circuit
-def dist_min(pos, point):
-    return min(np.sqrt((pos[0][i]-point[0])**2+(pos[1][i]-point[1])**2) for i in range(len(pos[0])))
-
-liste_points_gauche=np.stack((pos[0]-orth_normalise[0]*width , pos[1]-orth_normalise[1]*width))
-liste_points_droite=np.stack((pos[0]+orth_normalise[0]*width , pos[1]+orth_normalise[1]*width))
+#Détection des points trop proches du circuit
+def distance_min(pos, point):
+    #print(np.linalg.norm(pos.T-point, axis=1))
+    return np.min(np.linalg.norm(pos.T-point, axis=1))
 
 def points_gardes(pos, liste_points):
-    liste=[[],[]]
+    liste = []
     for i in range(len(pos[0])):
-        point=(liste_points[0][i], liste_points[1][i])
-        if abs(dist_min(pos,point)-0.5)<eps:
-            liste[0].append(point[0])
-            liste[1].append(point[1])
-    return liste
+        point = liste_points[:,i]
+        if abs(distance_min(pos,point)-rayon_circuit)<eps:
+            liste.append(point)
+    return np.asarray(liste).T
 
-bordure=(pos[0]+orth_normalise[0]*width , pos[1]+orth_normalise[1]*width)
+liste_points_gauche = np.stack((pos[0]- rayon_circuit*orth_normalise[0], pos[1] - rayon_circuit*orth_normalise[1]))
+liste_points_droite = np.stack((pos[0]+ rayon_circuit*orth_normalise[0], pos[1] + rayon_circuit*orth_normalise[1]))
 
-l=[dist_min(pos, point) for point in bordure]
-distances = np.asarray(l)
-plt.plot(bordure[distances > width * 0.98])
-
-
-def distance(t0, t1):
-   integrate.quad(lambda t: abs(f_prime(t)), t0, t1)
-   
-
-
-if __name__=="__main__":
-    test()                  #fonction test à definir
-
-
+liste_gauche_triee = points_gardes(pos, liste_points_gauche)
+liste_droite_triee = points_gardes(pos, liste_points_droite)
