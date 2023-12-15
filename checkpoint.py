7195550c0c5b276c55526410fb0a013949a29810
Cycynouche <151720950+Cycynouche@users.noreply.github.com>
2023-12-15 16:01:07 +0100

    Update checkpoint.py

--- checkpoint.py
+++ checkpoint.py
@@ -1,12 +1,33 @@
-import matplotlib.pyplot as plt
 import circuit
+from scipy.optimize import root_scalar
+import scipy.integrate as integrate
+import numpy as np
+
+#Nombre de checkpoints
+N=10
+
 
 abscisse_g = circuit.liste_gauche_triee[0]
 ordonnee_g = circuit.liste_gauche_triee[1]
 abscisse_d = circuit.liste_droite_triee[0]
 ordonnee_d = circuit.liste_droite_triee[1]
 
-checkpoint_gauche_abscisse = abscisse_g[0: len(abscisse_g): 80]
-checkpoint_gauche_ordonnee = ordonnee_g[0: len(ordonnee_g): 80]
-checkpoint_droit_abscisse = abscisse_d[0: len(abscisse_d): 80]
-checkpoint_droit_ordonnee = ordonnee_d[0: len(ordonnee_d): 80]
+
+
+def distance(t0, t1):
+   return integrate.quad(lambda t: np.linalg.norm(circuit.f_prime(t)), t0, t1)[0]
+   
+#Distance entre les checkpoints   
+D=distance(-1.5,1.5)/N
+
+def calcul_checkpoints(f_prime, td, ta, n):
+    L=[td]
+    t_prec=td
+    g=lambda t: distance(td, t)-D
+    for i in range(n-1):
+        xo_guess= t_prec + D
+        if xo_guess> ta:
+            xo_guess=(ta + t_prec)/2
+        t_prec= root_scalar(g, bracket=(t_prec,ta),fprime=lambda t:np.linalg.norm(f_prime(t)), xo=xo_guess)
+        L.append(t_prec)
+    return L
