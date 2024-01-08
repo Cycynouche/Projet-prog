5064d438a002162fcd5366531b5ffa761ebd6500
Klarinouche <151720588+Klarinouche@users.noreply.github.com>
2024-01-08 09:24:23 +0100

    Update affichage.py

--- affichage.py
+++ affichage.py
@@ -12,4 +12,6 @@ plt.plot(circuit.liste_droite_triee[0], circuit.liste_droite_triee[1])
 
 #checkpoints
 plt.scatter(checkpoint.x, checkpoint.y)
+plt.plot(checkpoint.liste_points_gauche[0], checkpoint.liste_points_gauche[1], '+')
+plt.plot(checkpoint.liste_points_droite[0], checkpoint.liste_points_droite[1], '+')
 plt.show()
