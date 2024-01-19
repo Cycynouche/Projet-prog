70d96e142b7bdae0c8d65dae057000caf948cf48
Cycynouche <151720950+Cycynouche@users.noreply.github.com>
2024-01-19 13:31:55 +0100

    Create gtrajectoire.py

---
+++ gtrajectoire.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+
+
+#fonction qui prend un vecteur(t) et renvoie un vecteur (f)
+
+    
+def calcul_f(t, D, G):
+    P= D+np.reshape(t,(len(t),1))*(G-D)
+    result=np.linalg.norm(P[1:]-P[:-1], axis=1)
+    return result
