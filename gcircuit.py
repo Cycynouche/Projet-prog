9b3d77b14235438498a8a5c54b264c2dbd95e114
ninamas <151721366+ninamas@users.noreply.github.com>
2024-01-19 14:16:45 +0100

    Update gcircuit.py

--- gcircuit.py
+++ gcircuit.py
@@ -3,7 +3,7 @@ import numpy as np
 #Détection des points trop proches du circuit
 def distance_min(pos, point):
     return np.min(np.linalg.norm(pos.T-point, axis=1))    
-#axis=1 spécifie que la norme doit être calculée le long de l'axe 1, qui correspond aux lignes
+    #axis=1 spécifie que la norme doit être calculée le long de l'axe 1, qui correspond aux lignes
 
 
 def points_gardes(pos, liste_points, rayon_circuit):
